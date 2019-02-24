16cce76d96568a16674475bc07efe16bbda25b3d
Umair Naushad <Umair.Naushad@emaratech.ae>
2019-02-25 00:31:49 +0400

    Nexus

---
+++ Maven_TestNG_Nexus/src/test/java/Test/TestCasesClass2.java
@@ -0,0 +1,13 @@
+package Test;
+import org.testng.annotations.Test;
+
+public class TestCasesClass2 {
+  @Test
+  public void testMethod1() {
+	  System.out.println("Test Class 2 - Test Method 1");
+  }
+  @Test
+  public void testMethod2() {
+	  System.out.println("Test Class 2 - Test Method 2");
+  }
+}
